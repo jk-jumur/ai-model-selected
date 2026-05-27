e1fc9f7eab8a74c5471c9bfed5c4977d4251a362
jk-jumur <developer.jumur@gmail.com>
2026-05-28 02:25:44 +0600

    navbar create

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 
 import './App.css'
+import Banner from './Components/Banner'
+import Footer from './Components/Footer'
+import NavBar from './Components/NavBar'
 
 function App() {
   
@@ -7,7 +10,9 @@ function App() {
   return (
     <>
   
-     
+        <NavBar/>
+        <Banner/>
+        <Footer/>
      
 
    
